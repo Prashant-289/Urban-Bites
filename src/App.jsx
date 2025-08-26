2ab6103176c1b36a4f5e07821e8413fbade292fc
Prashant Kali <you@example.com>
2025-08-27 00:48:52 +0530

    Implement FoodDisplay and FoodItem components with styling; integrate StoreContext for food list management

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Home from './pages/Home/Home'
 const App = () => {
   return (
     <div className='app'>
-		<Navbar/>	
+		<Navbar/>
 		<Routes>
 			<Route path= '/' element ={<Home/>}/>
 			<Route path= '/cart' element ={<Cart/>}/>
