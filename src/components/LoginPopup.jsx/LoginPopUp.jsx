1c73314c772e06b3e244794146671dda0721577f
Prashant Kali <you@example.com>
2025-09-01 21:08:53 +0530

    Implement login popup and integrate with Navbar; enhance Cart component with item display and total calculation

---
+++ src/components/LoginPopup.jsx/LoginPopUp.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import './LoginPopUp.css'
+import { assets } from '../../assets/assets'
+
+const LoginPopUp = ({setShowLogin}) => {
+    const[currState, setCurrState] = useState("Login")
+  return (
+    <div className='login-popup'>
+      <form className='login-popup-container'>
+        <div className="login-popup-title">
+            <h2>{currState}</h2>
+            <img onClick={() => setShowLogin(false)} src={assets.cross_icon} alt=''/>
+        </div>
+        <div className="login-popup-inputs">
+            {currState === "Login"? <></> : <input type='text' placeholder='Your Full Name' required/>}
+            
+            <input type='email' placeholder='Your Email' required/>
+            <input type='password' placeholder='Password' required/>
+        </div>
+        <button>{currState ==='Sign Up' ? "Create account" : "login"}</button>
+        <div className="login-popup-condition">
+            <input type='checkbox' required />
+            <p>By continuin, i agree to the tearms of use & privacy policies</p>
+        </div>
+        {currState === "Login"? 
+        <p>Create a new account? <span onClick={() => setCurrState("Sign Up")}>Click here</span></p> : 
+        <p>Already have an account <span onClick={() => setCurrState("Login")}>Login here</span></p>}
+        
+        
+      </form>
+    </div>
+  )
+}
+
+export default LoginPopUp;
